8248ec422c6b0b9bbf79dd5e0cdf1dd3411d4b5f
GuoZhong196904 <1969049380@qq.com>
2018-11-28 14:40:15 +0800

    1

--- src/main/java/com/testGit/Test01.java
+++ src/main/java/com/testGit/Test01.java
@@ -2,9 +2,10 @@ package com.testGit;
 
 public class Test01 {
 public static void main(String[] args) {
-	System.out.println("1");
+	System.out.println("3");
 	System.out.println("1");
 	System.out.println("1");
 	System.out.println("2");
+	System.out.println("hot_fix");
 }
 }
